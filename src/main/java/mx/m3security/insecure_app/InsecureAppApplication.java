6c81ebb100d098cda841f878191c7690a150b03b
José Eduardo Olvera Varillas <jolvera@m3security.mx>
2026-06-16 15:55:17 -0600

    fixes

---
+++ src/main/java/mx/m3security/insecure_app/InsecureAppApplication.java
@@ -0,0 +1,13 @@
+package mx.m3security.insecure_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsecureAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/m3security/insecure_app/InsecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.m3security.insecure_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
